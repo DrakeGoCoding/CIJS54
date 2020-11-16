93c585149abb36657826564a62fb5843746c070f
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-11-17 00:28:02 +0700

    hws1

--- proposal/proposal.js
+++ proposal/proposal.js
@@ -2,6 +2,6 @@ let answersDiv = document.querySelector('.answers');
 let yesDiv = document.querySelector('#yes')
 let noDiv = document.querySelector('#no');
 
-yesDiv.onclick = () => alert('Congratulation!')
+yesDiv.onclick = () => alert('Congratulation!');
 
 noDiv.onmouseover = () => answersDiv.classList.toggle('reverse');
