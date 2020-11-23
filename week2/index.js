33760e3ababd979afb7fcc61b37d17d1a2b2b318
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-11-23 20:54:11 +0700

    nah

--- week2/index.js
+++ week2/index.js
@@ -14,10 +14,41 @@ class Person {
     }
 }
 
+class Student extends Person {
+    university;
+    major;
+
+    constructor(name, age, address, university, major) {
+        super(name, age, address);
+        this.university = university;
+        this.major = major;
+    }
+
+    introduce() {
+        super.introduce();
+        console.log(`And I'm a student of ${this.major} at ${this.university}`);
+    }
+}
+
+class MyMath {
+    static sum(a, b) {
+        return a + b;
+    }
+    
+    subtract(a, b) {
+        return a - b;
+    }
+}
+
 const LONG = new Person('Long', 20, 'Thai Binh');
 console.log(LONG);
 LONG.introduce();
 
-const HA = new Person('Ha', 20, 'Ha Noi');
-console.log(HA);
-HA.introduce();
+const ANOTHER_LONG = new Student('Long', 20, 'Thai Binh', 'Hanoi University Of Science and Technology', 'Global ICT');
+console.log(ANOTHER_LONG);
+ANOTHER_LONG.introduce();
+
+console.log(MyMath.sum(1, 2));
+
+// this shows error
+// console.log(MyMath.subtract(1, 2));
