c56e70e2c06836a90c9fc30b9972b51f4ce5ece5
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-11-26 20:01:36 +0700

    module

---
+++ week3/index.js
@@ -0,0 +1,9 @@
+// To import specific functions
+// import { greet, endGreet } from "./module.js";
+// greet();
+// endGreet();
+
+// To import all files
+import * as module from "./module.js"
+module.greet()
+module.endGreet()
---
+++ week3/module.js
@@ -0,0 +1,17 @@
+// function greet() {
+//     console.log("hello world");
+// }
+
+// function endGreet(){
+//     console.log("goodbye world");
+// }
+
+// export { greet, endGreet }
+
+// Similar
+export function greet(){
+    console.log("hello world");
+}
+export function endGreet(){
+    console.log("goodbye world");
+}
