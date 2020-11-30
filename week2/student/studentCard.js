8e73bb46c68a0aa2809ad750ee43da6dd6a5d84f
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-11-30 20:02:02 +0700

    callback

--- week2/student/studentCard.js
+++ week2/student/studentCard.js
@@ -2,17 +2,35 @@ export default class StudentCard extends HTMLElement {
     constructor() {
         super();
         this.shadowDom = this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback(){
         this.name = this.getAttribute('name');
         this.age = this.getAttribute('age');
         this.pob = this.getAttribute('pob');
         this.shadowDom.innerHTML = `
-        ${STYLE}
-        <div class="student">
-            <p class="student-name"><strong>Name:</strong> ${this.name}</p>
-            <p class="student-age"><strong>Age:</strong> ${this.age}</p>
-            <p class="student-POB"><strong>Place of birth:</strong> ${this.pob}</p>
-        </div>
+            ${STYLE}
+            <div class="student">
+                <p class="student-name"><strong>Name:</strong> ${this.name}</p>
+                <p class="student-age"><strong>Age:</strong> ${this.age}</p>
+                <p class="student-POB"><strong>Place of birth:</strong> ${this.pob}</p>
+            </div>
         `;
+        console.log(`a student-card ${this.name, this.age, this.pob} is generated`);
+    }
+
+    disconnectedCallback(){
+        console.log(`a student-card ${this.name, this.age, this.pob} is removed`);
+    }
+
+    static get observedAttributes(){
+        return ['name', 'age', 'pob'];
+    }
+
+    attributeChangedCallback(attribute, oldValue, newValue){
+        console.log(`attribute = ${attribute}`);
+        console.log(`old value = ${oldValue}`);
+        console.log(`new value = ${newValue}`);
     }
 }
 
