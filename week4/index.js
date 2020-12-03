dd767c215a72d3b32a7b66f4d4b62aa17d226ef8
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-12-03 20:32:52 +0700

    nah

--- week4/index.js
+++ week4/index.js
@@ -10,20 +10,56 @@ async function getUserDocumentByID(id){
     console.log(user);
 }
 
+// getUserDocumentByID('1QxoZXZFUPzIdcFwyRlJ');
+// getUserDocumentByID('NStmi4kg2qDrI5tNwRo3');
+
 async function getUserDocuments(){
     const res = await firebase.firestore().collection('users').get();
     let users = getDataFromDocs(res.docs);
     users.forEach(user => console.log(user));
 }
 
-function addUserDocument(data){
+// getUserDocuments();
+
+function addDocument(data){
     firebase.firestore().collection('users').add(data);
 }
 
-function updateUserDocumentById(id, newData){
+// const data = {
+//     name: 'Paul Templeton',
+//     age: 28,
+//     phoneNumber: '0234576934'
+// }
+
+// addDocument(data);
+
+function updateDocumentById(id, newData){
     firebase.firestore().collection('users').doc(id).update(newData);
 }
 
-function deleteUserDocumentById(id){
+// const id = '1QxoZXZFUPzIdcFwyRlJ';
+
+// const newData = {
+//     name: 'Alice Simpson',
+//     age: 24,
+//     phoneNumber: '0234869372'
+// }
+
+// if only some fields need changing, just pass the desired fields
+// const newData = {
+//     name: 'Alice Beverly',
+//     phoneNumber: '0234869372'
+// }
+
+// if a field needs to append a new value
+// const newData = {
+//     phoneNumber: firebase.firestore.FieldValue.arrayUnion('0346178433')
+// }
+
+// updateDocumentById(id, newData);
+
+function deleteDocument(id){
     firebase.firestore().collection('users').doc(id).delete();
 }
+
+// deleteDocument('RUUbazVly5yBKrJ1HRGa');
