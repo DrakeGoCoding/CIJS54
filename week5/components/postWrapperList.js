7166dd1c75277839c2b3b4a51df5a171df4fffd6
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-12-17 20:09:51 +0700

    nah

--- week5/components/postWrapperList.js
+++ week5/components/postWrapperList.js
@@ -1,9 +1,9 @@
-import { getItemFromLocalStorage, compareCreatedDate, getPublicPostDocuments } from '../utils.js'
+import { getDataFromDoc, getItemFromLocalStorage, compareCreatedDate, getPublicPostDocuments } from '../utils.js'
 
 export class PostWrapperList extends HTMLElement {
     constructor() {
         super();
-        this.shadowDom = this.attachShadow({ mode: 'open' });
+        this.shadowDom = this.attachShadow({ mode: 'open' });      
     }
 
     async connectedCallback() {
@@ -33,10 +33,29 @@ export class PostWrapperList extends HTMLElement {
         let firstRun = true;
         firebase.firestore()
             .collection('posts')
+            .where('isPublic', '==', true)
             .onSnapshot(snapShot => {
                 if (firstRun) firstRun = false;
-                else console.log('Snapshot', snapShot.docChanges());
+                else{
+                    const docChanges = snapShot.docChanges();
+                    for (const docChange of docChanges){
+                        if (docChange.type === 'added'){
+                            this.appendNewPost(getDataFromDoc(docChange.doc));
+                        }
+                    }
+                }
             });
+    }
+
+    appendNewPost(data){
+        const currentUser = getItemFromLocalStorage('currentUser');
+        const newPostWrapper = document.createElement("post-wrapper");
+        newPostWrapper.setAttribute('user-name', currentUser.fullName);
+        newPostWrapper.setAttribute('created-time', data.createdDate);
+        newPostWrapper.setAttribute('content', data.content);
+        
+        const postList = this.shadowDom.querySelector('.post-list');
+        postList.insertBefore(newPostWrapper, postList.firstChild);
     }
 }
 
